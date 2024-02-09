1af7c290b92049b450bb01cd1097ce1e13d87062
aluipio <aluipio@gmail.com>
2024-02-08 21:52:52 -0300

    Alterações OP_1

--- library/Calculadora.py
+++ library/Calculadora.py
@@ -1,2 +1,16 @@
 class Calculadora:
-    pass
+
+    def __init__(self):
+        pass
+
+    # Recebe dois números e retorna a soma
+    def soma(self, x, y):
+        return x+y
+
+    # Recebe dois números e retorna o produto
+    def multiplicacao(self, x, y):
+        return x*y
+
+    # Recebe dois números e retorna a divisão do primeiro pelo segundo
+    def divisao(self, x, y):
+        return x/y
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from library.Calculadora import Calculadora
 
 calc = Calculadora()
+print(calc.soma(1,1))
 
 if __name__ == '__main__':
     print('Iniciando')
