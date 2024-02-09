92d9d4e0c653f7fdf2141faf40f042b42bb1e82a
aluipio <aluipio@gmail.com>
2024-02-08 21:55:02 -0300

    Atualização 3

--- library/Calculadora.py
+++ library/Calculadora.py
@@ -14,3 +14,7 @@ class Calculadora:
     # Recebe dois números e retorna a divisão do primeiro pelo segundo
     def divisao(self, x, y):
         return x/y
+    
+    # Recebe dois números e retorna a divisão do primeiro pelo segundo
+    def subtracao(self, x, y):
+        return x-y    
